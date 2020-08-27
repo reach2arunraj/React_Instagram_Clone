9ddb9434e6d07883283caddf7b89c95b36028ef9
reach2arunraj <60217918+reach2arunraj@users.noreply.github.com>
2020-08-27 15:26:18 +0530

    Firebase Database config

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import './App.css';
 import Post from "./Post"
+import { db } from "./firebase"
 
 function App() {
 
-  const [posts, setPosts] = useState([
-    {
-    imageUrl: "https://myhero.com/images/guest/g282317/hero105677/image2.jpg",
-    username:"arunsmith60",
-    caption:"WOW its Hermoine"
-  },
-  {
-    imageUrl: "https://media1.popsugar-assets.com/files/thumbor/fAzDfg5bOasPxyoo9A4CPbgvxWw/660x0:1312x967/fit-in/728xorig/filters:format_auto-!!-:strip_icc-!!-/2020/02/28/913/n/1922283/d41abaac5e597e33a9d771.12680042_/i/Harry-Potter-Quotes-Quiz.jpg",
-    username:"Deepak",
-    caption:"WOW its HArry potter"
-  }
-])
+  const [posts, setPosts] = useState([])
+
+useEffect(() =>{
+  db.collection("posts").onSnapshot(snapshot =>{
+    // Whenever a new post added, this code is triggered
+    setPosts(snapshot.docs.map(doc => ({
+      id:doc.id,
+      post: doc.data()
+    })))
+  })
+},[])
   
 
   return (
@@ -30,12 +30,11 @@ function App() {
       
       <h1>Hello World this is Arun from India, This is my React Instagram clone Application...</h1>
       {
-        posts.map(post =>(
-          <Post username={post.username} imageUrl={post.imageUrl} caption={post.caption} />
+        posts.map(({id, post}) =>(
+          <Post key={id} username={post.username} imageUrl={post.imageUrl} caption={post.caption} />
         ))
       }
     </div>
-
   );
 }
 
